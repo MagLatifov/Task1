d1c4de758c7c5eccf31e1bba4f6ffd8c5e008bd8
MagLatifov <mag.latifov@yandex.ru>
2025-12-03 15:26:04 +0300

    Исправлены замечания Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class Main {
@@ -18,12 +21,19 @@ public class Main {
         userService.saveUser("Magomed", "Magomedovich", (byte)34);
         userService.saveUser("Patimat", "Kalimatovna", (byte)56);
         userService.saveUser("Ramazan", "Perbudagovich", (byte)45);
-        System.out.println(userService.getAllUsers());
+        printList(userService.getAllUsers());
         userService.removeUserById(3);
-        System.out.println(userService.getAllUsers());
+        printList(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
         Util.shutDownSessionFactory();
+    }
 
+    public static void printList(List<User> usr) {
+        int i = 1;
+        for (User user : usr) {
+            log.info("Запись #{}: {}", i, user);
+            i++;
+        }
     }
 }
